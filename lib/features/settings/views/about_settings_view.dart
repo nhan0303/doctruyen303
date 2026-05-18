e1c48ee28c3e388a12cf5fde1f88cc33f76bc3cf
nhan0303 <nhanla.22git@vku.udn.vn>
2026-05-19 02:02:27 +0700

    change button

--- lib/features/settings/views/about_settings_view.dart
+++ lib/features/settings/views/about_settings_view.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_manga_reader/core/extensions/build_context_extensions.dart';
 import 'package:flutter_manga_reader/core/utils/consts.dart';
 import 'package:flutter_manga_reader/core/widgets/app_asset_image.dart';
-import 'package:flutter_manga_reader/design_system/icons/manga_reader_icons.dart';
 import 'package:flutter_manga_reader/features/settings/widgets/generic_settings_view.dart';
 import 'package:flutter_manga_reader/gen/assets.gen.dart';
 import 'package:flutter_manga_reader/src/version.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 class AboutSettingsView extends StatelessWidget {
   const AboutSettingsView({super.key});
@@ -26,11 +24,7 @@ class AboutSettingsView extends StatelessWidget {
           height: 80,
         ),
       ),
-      children: const [
-        _VersionTile(),
-        _OpenSourceLicenses(),
-        _SocialLinks(),
-      ],
+      children: const [_VersionTile(), _OpenSourceLicenses()],
     );
   }
 }
@@ -55,54 +49,8 @@ class _OpenSourceLicenses extends StatelessWidget {
     return ListTile(
       title: Text(context.strings.settings_about_open_source_licenses),
       onTap: () {
-        showLicensePage(
-          context: context,
-          applicationName: Consts.appName,
-        );
+        showLicensePage(context: context, applicationName: Consts.appName);
       },
     );
   }
-}
-
-class _SocialLinks extends StatelessWidget {
-  const _SocialLinks();
-
-  @override
-  Widget build(BuildContext context) {
-    const links = <_SocialLinkData>[
-      _SocialLinkData(
-        icon: Icons.public,
-        link: 'https://rouxguillau.me',
-      ),
-      _SocialLinkData(
-        icon: MangaReader.twitter,
-        link: 'https://twitter.com/TesteurManiak',
-      ),
-      _SocialLinkData(
-        icon: MangaReader.github,
-        link: 'https://github.com/TesteurManiak',
-      ),
-    ];
-
-    return Wrap(
-      alignment: WrapAlignment.center,
-      children: [
-        for (final link in links)
-          IconButton(
-            icon: Icon(link.icon),
-            onPressed: () => launchUrlString(link.link),
-          ),
-      ],
-    );
-  }
-}
-
-class _SocialLinkData {
-  const _SocialLinkData({
-    required this.icon,
-    required this.link,
-  });
-
-  final IconData icon;
-  final String link;
 }
